e71447a2ae6b270e8f10fd0030994cf629325e9b
Jenna <jenna48071@gmail.com>
2023-03-30 16:57:21 -0500

    added win and lose outcome

--- script.js
+++ script.js
@@ -8,21 +8,31 @@ const answers = [
   "tombstone",
   "hocus-pocus",
 ];
-const button = document.getElementById("btn");
+const againBtn = document.getElementById("playAgain");
+const playAgainContainer = document.getElementById("playAgain");
 const answer = document.getElementById("answer");
 const lives = document.getElementById("lives");
 const alphabetContainer = document.getElementById("alphabetContainer");
+const revealAnswer = document.getElementById("revealAnswer");
 
-let currentAnswer = answers[Math.floor(Math.random() * answers.length)];
-let answerSplit = currentAnswer.split("");
+let currentAnswer = "";
+let answerSplit;
 let Includes = false;
-let guesses = [];
+let counter = 0;
+let correctGuesses = [];
 let currentGuess;
-let life = 10;
+let life;
 let alphabet;
 let guess;
+let disabled = false;
 
 const createBoard = () => {
+  // DEFAULT INFO
+  currentAnswer = answers[Math.floor(Math.random() * answers.length)];
+  answerSplit = currentAnswer.split("");
+  life = 10;
+  lives.innerHTML = `Lives Remaining: ${life}`;
+
   // DISPLAY CHOICES
   alphabet = "abcdefghijklmnopqrstuvwxyz"
     .split("")
@@ -31,8 +41,8 @@ const createBoard = () => {
     )
     .join("");
   alphabetContainer.innerHTML = alphabet;
-  lives.innerHTML = `Lives Remaining: ${life}`;
-  // DISPLAYED CENSORED ANSWER
+
+  // DISPLAY CENSORED ANSWER
   for (let i = 0; i < currentAnswer.length; i++) {
     guess = document.createElement("p");
     guess.setAttribute("id", `guess${i}`);
@@ -41,6 +51,7 @@ const createBoard = () => {
       guess.innerHTML = "__";
     } else {
       guess.innerHTML = "-";
+      counter += 1;
     }
     answer.appendChild(guess);
   }
@@ -50,29 +61,47 @@ alphabetContainer.addEventListener("click", (e) => {
   currentGuess = e.target.id;
   let letters = document.getElementById(e.target.id);
   Includes = answerSplit.includes(currentGuess);
-  console.log(Includes);
-  //Change button color if correct
-  if (Includes === true) {
-    letters.classList.add("correct");
-  } else {
-    letters.classList.add("incorrect");
-    //Remove Life
-    if (life <= 1) {
-      alert("You Lose!");
-    } else {
-      life -= 1;
-      lives.innerHTML = `Lives Remaining: ${life}`;
-    }
-  }
+
   //Reveal correct letter
   for (let i = 0; i < currentAnswer.length; i++) {
     let g = document.getElementById(`guess${i}`);
     if (answerSplit[i] === currentGuess) {
       g.innerHTML = currentGuess;
+      counter += 1;
+    }
+  }
+
+  const outcome = (outcome) => {
+    if (outcome === "Lose") {
+      life = 0;
+      lives.innerHTML = `Lives Remaining: ${life}`;
+      revealAnswer.innerHTML = currentAnswer;
+      revealAnswer.classList.remove("hidden");
+    }
+
+    alphabetContainer.classList.add("disabled");
+    againBtn.classList.remove("hidden");
+  };
+
+  //Change button color if correct
+  if (Includes === true) {
+    letters.classList.add("correct");
+    if (counter === currentAnswer.length) {
+      outcome("Win");
+    }
+  } else {
+    letters.classList.add("incorrect");
+    letters.disabled = true;
+    //Remove Life
+    if (life <= 1) {
+      outcome("Lose");
     } else {
-      console.log("no");
+      life -= 1;
+      lives.innerHTML = `Lives Remaining: ${life}`;
     }
   }
 });
+
+againBtn.addEventListener("click", () => location.reload());
 
 window.onload = createBoard();
