7d6f4da98059c86fea8dc3a56517424072d776fa
Jenna <jenna48071@gmail.com>
2023-03-30 18:53:13 -0500

    added hangman

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const answer = document.getElementById("answer");
 const lives = document.getElementById("lives");
 const alphabetContainer = document.getElementById("alphabetContainer");
 const revealAnswer = document.getElementById("revealAnswer");
+const canvas = document.getElementById("hangman");
+const context = canvas.getContext("2d");
 
 let currentAnswer = "";
 let answerSplit;
@@ -34,7 +36,7 @@ const createBoard = () => {
   // DEFAULT INFO
   currentAnswer = answers[Math.floor(Math.random() * answers.length)];
   answerSplit = currentAnswer.split("");
-  life = 10;
+  life = 9;
   lives.innerHTML = `Lives Remaining: ${life}`;
 
   // DISPLAY CHOICES
@@ -59,6 +61,98 @@ const createBoard = () => {
     }
     answer.appendChild(guess);
   }
+};
+
+//DRAW HANGMAN
+Draw = (part) => {
+  switch (part) {
+    case "gallows":
+      context.strokeStyle = "#444";
+      context.lineWidth = 10;
+      context.beginPath();
+      context.moveTo(175, 225);
+      context.lineTo(5, 225);
+      context.moveTo(40, 225);
+      context.lineTo(25, 5);
+      context.lineTo(100, 5);
+      context.lineTo(100, 25);
+      context.stroke();
+      break;
+
+    case "head":
+      context.lineWidth = 5;
+      context.beginPath();
+      context.arc(100, 50, 25, 0, Math.PI * 2, true);
+      context.closePath();
+      context.stroke();
+      break;
+
+    case "body":
+      context.beginPath();
+      context.moveTo(100, 75);
+      context.lineTo(100, 140);
+      context.stroke();
+      break;
+
+    case "rightHarm":
+      context.beginPath();
+      context.moveTo(100, 85);
+      context.lineTo(60, 100);
+      context.stroke();
+      break;
+
+    case "leftHarm":
+      context.beginPath();
+      context.moveTo(100, 85);
+      context.lineTo(140, 100);
+      context.stroke();
+      break;
+
+    case "rightLeg":
+      context.beginPath();
+      context.moveTo(100, 140);
+      context.lineTo(80, 190);
+      context.stroke();
+      break;
+
+    case "rightFoot":
+      context.beginPath();
+      context.moveTo(82, 190);
+      context.lineTo(70, 185);
+      context.stroke();
+      break;
+
+    case "leftLeg":
+      context.beginPath();
+      context.moveTo(100, 140);
+      context.lineTo(125, 190);
+      context.stroke();
+      break;
+
+    case "leftFoot":
+      context.beginPath();
+      context.moveTo(122, 190);
+      context.lineTo(135, 185);
+      context.stroke();
+      break;
+  }
+};
+const draws = [
+  "gallows",
+  "head",
+  "body",
+  "rightHarm",
+  "leftHarm",
+  "rightLeg",
+  "leftLeg",
+  "rightFoot",
+  "leftFoot",
+];
+var step = 0;
+
+const addBody = () => {
+  Draw(draws[step++]);
+  if (undefined === draws[step]) this.disabled = true;
 };
 
 alphabetContainer.addEventListener("click", (e) => {
@@ -98,8 +192,10 @@ alphabetContainer.addEventListener("click", (e) => {
     letters.disabled = true;
     //Remove Life
     if (life <= 1) {
+      addBody();
       outcome("Lose");
     } else {
+      addBody();
       life -= 1;
       lives.innerHTML = `Lives Remaining: ${life}`;
     }
@@ -107,5 +203,4 @@ alphabetContainer.addEventListener("click", (e) => {
 });
 
 againBtn.addEventListener("click", () => location.reload());
-
 window.onload = createBoard();
